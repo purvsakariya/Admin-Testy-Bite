94d47a5212cdef0b78003f8d3fc1bbb15ab94af9
Purv Sakariya <purvsakariya134@gmail.com>
2026-05-21 17:19:50 +0530

    Add Menu svg in dashboard

--- frontend/src/Components/DashBoard.jsx
+++ frontend/src/Components/DashBoard.jsx
@@ -5,13 +5,13 @@ import Loader from './Loader.jsx';
 
 function DashBoard() {
   const navigate = useNavigate();
-  const { 
-    orders, 
-    users, 
-    availableMeals, 
-    ordersLoading, 
-    usersLoading, 
-    mealsLoading 
+  const {
+    orders,
+    users,
+    availableMeals,
+    ordersLoading,
+    usersLoading,
+    mealsLoading
   } = useContext(Context);
 
   if (ordersLoading || usersLoading || mealsLoading) {
@@ -116,9 +116,14 @@ function DashBoard() {
 
         <div className="stat-card" onClick={() => navigate("/meals")}>
           <div className="stat-icon-wrapper">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
-              <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-clipboard-list">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+              <path d="M9 5a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2" />
+              <path d="M9 12l.01 0" />
+              <path d="M13 12l2 0" />
+              <path d="M9 16l.01 0" />
+              <path d="M13 16l2 0" />
             </svg>
           </div>
           <div className="stat-info">
